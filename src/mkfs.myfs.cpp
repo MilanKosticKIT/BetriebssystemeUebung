4496f49abaec568555d68510274478240cbce3ae
Milan Kostić <allgemein@milan-kostic.de>
2018-05-03 14:49:23 +0200

    Added constants for part-size and start.

--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -10,6 +10,19 @@
 #include "blockdevice.h"
 #include "macros.h"
 
+#define SUPER_START 0
+#define DMAP_START 1
+#define FAT_START 17
+#define ROOT_START 273
+#define DATA_START 337
+
+#define SUPER_SIZE 1
+#define DMAP_SIZE = FAT_START - DMAP_START
+#define FAT_SIZE = ROOT_START - FAT_START
+#define ROOT_SIZE = DATA_START - ROOT_START
+#define DATA_SIZE 65536
+
+
 int main(int argc, char *argv[]) {
 
     // TODO: Implement file system generation & copying of files here
