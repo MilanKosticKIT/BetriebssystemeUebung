a2b5e54adb0a9951a21d2024910f2eb93c2744b8
Hannes Illy <Hannes.Illy@gmx.de>
2018-10-30 15:42:42 +0100

    Improved and tested template-IO-Methods to support multiple blocks.

--- src/FilesystemIO.cpp
+++ src/FilesystemIO.cpp
@@ -25,27 +25,43 @@ void FilesystemIO::writeDevice(std::size_t block, const T &data) {
 
 void FilesystemIO::writeDevice(std::size_t block, const T (&data)[N]) {
     static_assert(std::is_trivially_copyable<T>::value, "Can't operate on complex types!");
-    static_assert(sizeof(T) * N <= BLOCK_SIZE, "");
+
+    const char *rawData = reinterpret_cast<const char *>(&data);
 
     static char buffer[BLOCK_SIZE];
-    std::memcpy(buffer, data, sizeof(T) * N);
-    blockDevice.write(block, buffer);
+    std::size_t blockCount = (sizeof(T) * N)/ BLOCK_SIZE;
+    std::size_t currentBlock = block;
+    for (; currentBlock < block + blockCount; ++currentBlock) {
+        blockDevice.write(currentBlock, rawData + ((currentBlock - block) * BLOCK_SIZE));
+    }
+    std::memcpy(buffer, rawData + ((currentBlock - block) * BLOCK_SIZE), (sizeof(T) * N) % BLOCK_SIZE);
+    blockDevice.write(currentBlock, buffer);
 }
 
 void FilesystemIO::readDevice(std::size_t block, T &data) {
     static_assert(std::is_trivially_constructible<T>::value, "Can't operate on complex types!");
-    static_assert(sizeof(T) <= BLOCK_SIZE, "");
 
+    char *rawData = reinterpret_cast<char *>(&data);
     static char buffer[BLOCK_SIZE];
-    blockDevice.read(block, buffer);
-    std::memcpy(data, buffer, sizeof(T));
+    std::size_t blockCount = sizeof(T) / BLOCK_SIZE;
+    std::size_t currentBlock = block;
+    for (; currentBlock < block + blockCount; ++currentBlock) {
+        blockDevice.read(currentBlock, rawData + ((currentBlock - block) * BLOCK_SIZE));
+    }
+    blockDevice.read(currentBlock, buffer);
+    std::memcpy(rawData + ((currentBlock - block) * BLOCK_SIZE), buffer, sizeof(T) % BLOCK_SIZE);
 }
 
 void FilesystemIO::readDevice(std::size_t block, T (&data)[N]) {
     static_assert(std::is_trivially_constructible<T>::value, "Can't operate on complex types!");
-    static_assert(sizeof(T) * N <= BLOCK_SIZE, "");
 
+    char *rawData = reinterpret_cast<char *>(&data);
     static char buffer[BLOCK_SIZE];
-    blockDevice.read(block, buffer);
-    std::memcpy(data, buffer, sizeof(T) * N);
+    std::size_t blockCount = (sizeof(T) * N) / BLOCK_SIZE;
+    std::size_t currentBlock = block;
+    for (; currentBlock < block + blockCount; ++currentBlock) {
+        blockDevice.read(currentBlock, rawData + ((currentBlock - block) * BLOCK_SIZE));
+    }
+    blockDevice.read(currentBlock, buffer);
+    std::memcpy(rawData + ((currentBlock - block) * BLOCK_SIZE), buffer, (sizeof(T) * N) % BLOCK_SIZE);
 }
--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -19,8 +19,9 @@
 #include <iostream>
 #include <cstring>
 #include <type_traits>
+#include <stdint.h>
 
-typedef struct{
+typedef struct {
     char name[NAME_LENGTH];
     off_t size;
     uid_t userID;
@@ -28,31 +29,38 @@ typedef struct{
     time_t last_time;
     time_t modi_time;
     time_t change_time;
-}fileStats;
+} fileStats;
 
 //MARK: - Methodenheader
 //DMAP
 //TODO: Root?
 MetaData getMetaData(u_int8_t indexOfFile);
+
 void setMetaData(MetaData metaData, u_int8_t indexOfFile);
-void convertBlockToMetaData(MetaData* data, char* block);
-void convertMetaDataToBlock(MetaData* data, char* block);
-void getBLockFromAddress(u_int32_t address, u_int32_t* blockNo, u_int32_t* byteNo);
+
+void convertBlockToMetaData(MetaData *data, char *block);
+
+void convertMetaDataToBlock(MetaData *data, char *block);
+
+void getBLockFromAddress(u_int32_t address, u_int32_t *blockNo, u_int32_t *byteNo);
+
 //Superblock
-void getSuperBlock(SuperBlock* superblock);
+void getSuperBlock(SuperBlock *superblock);
+
 void setEmptySpaceSizeInSuperBlock(uint32_t emptySpaceSize);
+
 void createSuperBlock(struct SuperBlock superBlock);
 
 //TODO: Get these lines to a fitting header file.
 //MARK: -
 
-BlockDevice blockDevice = *new BlockDevice();
+BlockDevice blockDevice = BlockDevice();
 
 
-bool getStats(fileStats *status, char *path, blkcnt_t *blockCount){
+bool getStats(fileStats *status, char *path, blkcnt_t *blockCount) {
     struct stat sb;
-    stat(path,&sb);
-    char* filename = basename(path);
+    stat(path, &sb);
+    char *filename = basename(path);
     if (strlen(filename) > NAME_LENGTH) {
         return false;
     }
@@ -66,62 +74,81 @@ bool getStats(fileStats *status, char *path, blkcnt_t *blockCount){
     *blockCount = sb.st_blocks;
     return true;
 }
-void updateRoot(fileStats *status){
+
+void updateRoot(fileStats *status) {
     fileStats root[NUM_DIR_ENTRIES];
-    
-    
-    
-    
+
+
 }
 
-template <class T> void writeDevice(std::size_t block, const T& data) {
+template<class T>
+void writeDevice(std::size_t block, const T &data) {
     static_assert(std::is_trivially_copyable<T>::value, "Can't operate on complex types!");
-    
-    const char* rawData = reinterpret_cast<const char*>(&data);
-    
+
+    const char *rawData = reinterpret_cast<const char *>(&data);
+
     static char buffer[BLOCK_SIZE];
     std::size_t blockCount = sizeof(T) / BLOCK_SIZE;
     std::size_t currentBlock = block;
-    for(; currentBlock < block + blockCount; ++currentBlock) {
+    for (; currentBlock < block + blockCount; ++currentBlock) {
         blockDevice.write(currentBlock, rawData + ((currentBlock - block) * BLOCK_SIZE));
     }
     std::memcpy(buffer, rawData + ((currentBlock - block) * BLOCK_SIZE), sizeof(T) % BLOCK_SIZE);
     blockDevice.write(currentBlock, buffer);
 }
 
-template <class T, std::size_t N> void writeDevice(std::size_t block, const T (&data)[N]) {
+template<class T, std::size_t N>
+void writeDevice(std::size_t block, const T (&data)[N]) {
     static_assert(std::is_trivially_copyable<T>::value, "Can't operate on complex types!");
-    static_assert(sizeof(T) * N <= BLOCK_SIZE, "");
-    
+
+    const char *rawData = reinterpret_cast<const char *>(&data);
+
     static char buffer[BLOCK_SIZE];
-    std::memcpy(buffer, data, sizeof(T) * N);
-    blockDevice.write(block, buffer);
+    std::size_t blockCount = (sizeof(T) * N)/ BLOCK_SIZE;
+    std::size_t currentBlock = block;
+    for (; currentBlock < block + blockCount; ++currentBlock) {
+        blockDevice.write(currentBlock, rawData + ((currentBlock - block) * BLOCK_SIZE));
+    }
+    std::memcpy(buffer, rawData + ((currentBlock - block) * BLOCK_SIZE), (sizeof(T) * N) % BLOCK_SIZE);
+    blockDevice.write(currentBlock, buffer);
 }
 
-template<class T> void readDevice(std::size_t block, T& data) {
+template<class T>
+void readDevice(std::size_t block, T &data) {
     static_assert(std::is_trivially_constructible<T>::value, "Can't operate on complex types!");
-    static_assert(sizeof(T) <= BLOCK_SIZE, "");
-    
+
+    char *rawData = reinterpret_cast<char *>(&data);
     static char buffer[BLOCK_SIZE];
-    blockDevice.read(block, buffer);
-    std::memcpy(data, buffer, sizeof(T));
+    std::size_t blockCount = sizeof(T) / BLOCK_SIZE;
+    std::size_t currentBlock = block;
+    for (; currentBlock < block + blockCount; ++currentBlock) {
+        blockDevice.read(currentBlock, rawData + ((currentBlock - block) * BLOCK_SIZE));
+    }
+    blockDevice.read(currentBlock, buffer);
+    std::memcpy(rawData + ((currentBlock - block) * BLOCK_SIZE), buffer, sizeof(T) % BLOCK_SIZE);
 }
 
-template<class T, std::size_t N> void readDevice(std::size_t block, T (&data)[N]) {
+template<class T, std::size_t N>
+void readDevice(std::size_t block, T (&data)[N]) {
     static_assert(std::is_trivially_constructible<T>::value, "Can't operate on complex types!");
-    static_assert(sizeof(T) * N <= BLOCK_SIZE, "");
-    
+
+    char *rawData = reinterpret_cast<char *>(&data);
     static char buffer[BLOCK_SIZE];
-    blockDevice.read(block, buffer);
-    std::memcpy(data, buffer, sizeof(T) * N);
+    std::size_t blockCount = (sizeof(T) * N) / BLOCK_SIZE;
+    std::size_t currentBlock = block;
+    for (; currentBlock < block + blockCount; ++currentBlock) {
+        blockDevice.read(currentBlock, rawData + ((currentBlock - block) * BLOCK_SIZE));
+    }
+    blockDevice.read(currentBlock, buffer);
+    std::memcpy(rawData + ((currentBlock - block) * BLOCK_SIZE), buffer, (sizeof(T) * N) % BLOCK_SIZE);
 }
 
 
 
 int main(int argc, char *argv[]) {
-    
+
     blockDevice.open("./Blockdevice.bin");
-    
+
     /*
     fileStats foobar;
     foobar.size = 1024;
@@ -132,13 +159,14 @@ int main(int argc, char *argv[]) {
     
     std::cout << bar.size << std::endl;
      */
-    
-    /*
+
+    /* Test for read/writeDevice for one block with char (1 byte)
     char input[6] = {'a', 'b', 'c', 'd', 'e', '\0'};
     char putput[BLOCK_SIZE];
     char output[6];
     char* outputString = (char*) output;
     */
+    /* Test for read/writeDevice for one block with int (>1 byte)
     int input[6] = {0, 1, 2, 3, 4, 5};
     int output[6];
     
@@ -150,10 +178,85 @@ int main(int argc, char *argv[]) {
     for(int i = 0; i< 6; i++){
         std::cout << output[i] << std::endl;
     }
-    
+    */
+
+    /* Test for read/writeDevice for multiple blocks with struct
+typedef struct {
+    uint64_t one;
+    uint64_t two;
+    uint64_t three;
+    uint64_t four;
+    uint64_t five;
+    uint64_t six;
+    uint64_t seven;
+    uint64_t eight;
+    uint64_t nine;
+    uint64_t ten;
+    uint64_t eleven;
+} testNumbers;
+
+typedef struct {
+    testNumbers before;
+    testNumbers first;
+    testNumbers second;
+    testNumbers third;
+    testNumbers fourth;
+    testNumbers fifth;
+    testNumbers after;
+} testData;
+
+    testData input;
+    testData output;
+    input.before = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    input.first = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    input.second = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+    input.third = {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3};
+    input.fourth = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
+    input.fifth = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+    input.after = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    writeDevice(2, input);
+    readDevice(2, output);
+
+    uint64_t size = sizeof(testData) / 8;
+    uint64_t *outputArray;
+    outputArray = reinterpret_cast<uint64_t *>(&output);
+    std::cout << "Read size: " << sizeof(testData) << std::endl;
+    for (uint64_t i = 0; i < size; i++) {
+        std::cout << outputArray[i];
+    }
+    std::cout << std::endl;
+    */
+    /* Test for read/writeDevice with multiple blocks and array.
+    uint64_t input[101];
+    for (int i = 0; i < 101; i++) {
+        input[i] = 0;
+    }
+    input[0] = 132456;
+    input[2] = 11111111111111;
+    input[54] = 321321654;
+    input[67] = 987654321;
+    input[91] = 999999999999;
+
+    uint64_t output[101];
+    writeDevice(5, input);
+    readDevice(5, output);
+
+    for (int i = 0; i < 101; i++) {
+        if (output[i] == 0) {
+            std::cout << output[i];
+        } else {
+            std::cout << '\n' << output[i] << '\n';
+        }
+    }
+    std::cout << std::endl;
+    */
+
+
+
     // TODO: Implement file system generation & copying of files here
     return 0;
-    
+
 }
 
 
@@ -173,7 +276,7 @@ int main(int argc, char *argv[]) {
 //Returns the metaData of the file behind the index.
 MetaData getMetaData(u_int8_t indexOfFile) {
     u_int32_t blockNo = ROOT_START + indexOfFile;
-    char* block;
+    char *block;
     blockDevice.read(blockNo, block);
     MetaData data;
     convertBlockToMetaData(&data, block);       //TODO: Pls check deferensation, (and spelling).
@@ -183,38 +286,37 @@ MetaData getMetaData(u_int8_t indexOfFile) {
 //Sets the MetaData of an file, which is refrenced via the index.
 void setMetaData(MetaData metaData, u_int8_t indexOfFile) {
     u_int32_t blockNo = ROOT_START + indexOfFile;
-    char* block;
+    char *block;
     convertBlockToMetaData(&metaData, block);   //TODO: Pls check deferensation, (and spelling).
     blockDevice.write(blockNo, block);
 }
 
 //Casts the given block to a given MetaData instance
-void convertBlockToMetaData(MetaData* data, char* block){
+void convertBlockToMetaData(MetaData *data, char *block) {
     //TODO: Implement this.
 }
 
 //Converts the given MetaData into a block, formatted as char*/String
-void convertMetaDataToBlock(MetaData* data, char* block){
+void convertMetaDataToBlock(MetaData *data, char *block) {
     //TODO: Implement this.
 }
 
 //Gets the block and the byte number of the given address
-void getBLockFromAddress(u_int32_t address, u_int32_t* blockNo, u_int32_t* byteNo) {
+void getBLockFromAddress(u_int32_t address, u_int32_t *blockNo, u_int32_t *byteNo) {
     //TODO: Check whether needed
     *blockNo = address / BLOCK_SIZE;
     *byteNo = address % BLOCK_SIZE;
 }
 
 
-
 //MARK: - Superblock
 //Returns the Superblock for the data system
-void getSuperBlock(SuperBlock* superblock){
+void getSuperBlock(SuperBlock *superblock) {
     //    TODO: Implement this.
-    char* block = (char*) malloc(BLOCK_SIZE);
+    char *block = (char *) malloc(BLOCK_SIZE);
     blockDevice.read(0, block);
-    
-    uint32_t* data = (uint32_t*) block;
+
+    uint32_t *data = (uint32_t *) block;
     superblock->fileSystemSize = *data;
     data++;
     superblock->emptySpaceSize = *data;
@@ -240,22 +342,22 @@ void getSuperBlock(SuperBlock* superblock){
 }
 
 //Sets the emptySpaceSize arttibute in the superblock
-void setEmptySpaceSizeInSuperBlock(uint32_t emptySpaceSize){
-    char* block = (char*) malloc(BLOCK_SIZE);
-    uint32_t* data = (uint32_t*) block;
-    
+void setEmptySpaceSizeInSuperBlock(uint32_t emptySpaceSize) {
+    char *block = (char *) malloc(BLOCK_SIZE);
+    uint32_t *data = (uint32_t *) block;
+
     data++;
     *data = emptySpaceSize;
-    
+
     blockDevice.write(0, block);
     free(data);
 }
 
 //Writes the part of the filesystem for the superblock for the first time. In particular: The not changing values are written.
-void createSuperBlock(struct SuperBlock superBlock){
-    char* block = (char*) malloc(BLOCK_SIZE);
-    uint32_t* data = (uint32_t*) block;
-    
+void createSuperBlock(struct SuperBlock superBlock) {
+    char *block = (char *) malloc(BLOCK_SIZE);
+    uint32_t *data = (uint32_t *) block;
+
     *data = superBlock.fileSystemSize;
     data++;
     *data = superBlock.emptySpaceSize;
@@ -278,7 +380,7 @@ void createSuperBlock(struct SuperBlock superBlock){
     data++;
     *data = superBlock.dataSize;
     data++;
-    
+
     blockDevice.write(0, block);
     free(data);
 }
