7033e8b1d9e00bd408ca9d2487c14575eda6c08a
MilanKosticKIT <allgemein@milan-kostic.de>
2018-10-31 13:37:42 +0100

    Corrected some issues. Not working by now. (3)

--- src/filesystemIO.cpp
+++ src/filesystemIO.cpp
@@ -9,37 +9,37 @@ FilesystemIO::FilesystemIO() {
     // todo ticket #17: create blockdevice or get blockdevice/path parameter?
 }
 
-void FilesystemIO::writeDevice(std::size_t block, const T &data) {
-    static_assert(std::is_trivially_copyable<T>::value, "Can't operate on complex types!");
+void FilesystemIO::writeDevice(size_t block, const T &data) {
+    static_assert(is_trivially_copyable<T>::value, "Can't operate on complex types!");
 
     const char *rawData = reinterpret_cast<const char *>(&data);
 
     static char buffer[BLOCK_SIZE];
-    std::size_t blockCount = sizeof(T) / BLOCK_SIZE;
-    std::size_t currentBlock = block;
+    size_t blockCount = sizeof(T) / BLOCK_SIZE;
+    size_t currentBlock = block;
     for (; currentBlock < block + blockCount; ++currentBlock) {
         blockDevice.write(currentBlock, rawData + ((currentBlock - block) * BLOCK_SIZE));
     }
-    std::memcpy(buffer, rawData + ((currentBlock - block) * BLOCK_SIZE), sizeof(T) % BLOCK_SIZE);
+    memcpy(buffer, rawData + ((currentBlock - block) * BLOCK_SIZE), sizeof(T) % BLOCK_SIZE);
     blockDevice.write(currentBlock, buffer);
 }
 
-void FilesystemIO::writeDevice(std::size_t block, const T (&data)[N]) {
-    static_assert(std::is_trivially_copyable<T>::value, "Can't operate on complex types!");
+void FilesystemIO::writeDevice(size_t block, const T (&data)[N]) {
+    static_assert(is_trivially_copyable<T>::value, "Can't operate on complex types!");
 
     const char *rawData = reinterpret_cast<const char *>(&data);
 
     static char buffer[BLOCK_SIZE];
-    std::size_t blockCount = (sizeof(T) * N)/ BLOCK_SIZE;
-    std::size_t currentBlock = block;
+    size_t blockCount = (sizeof(T) * N)/ BLOCK_SIZE;
+    size_t currentBlock = block;
     for (; currentBlock < block + blockCount; ++currentBlock) {
         blockDevice.write(currentBlock, rawData + ((currentBlock - block) * BLOCK_SIZE));
     }
-    std::memcpy(buffer, rawData + ((currentBlock - block) * BLOCK_SIZE), (sizeof(T) * N) % BLOCK_SIZE);
+    memcpy(buffer, rawData + ((currentBlock - block) * BLOCK_SIZE), (sizeof(T) * N) % BLOCK_SIZE);
     blockDevice.write(currentBlock, buffer);
 }
 
-void FilesystemIO::readDevice(std::size_t block, T &data) {
+void FilesystemIO::readDevice(size_t block, T &data) {
     static_assert(std::is_trivially_constructible<T>::value, "Can't operate on complex types!");
 
     char *rawData = reinterpret_cast<char *>(&data);
