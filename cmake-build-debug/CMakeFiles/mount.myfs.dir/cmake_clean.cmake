817f538312ea15f339bc1f63e8b1654eebc6dbe1
Major2828 <alex28-96@web.de>
2018-11-05 17:41:45 +0100

    Root Class should work now, mkmyfs is cleaned up

---
+++ cmake-build-debug/CMakeFiles/mount.myfs.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mount.myfs.dir/src/blockdevice.cpp.obj"
+  "CMakeFiles/mount.myfs.dir/src/myfs.cpp.obj"
+  "CMakeFiles/mount.myfs.dir/src/wrap.cpp.obj"
+  "CMakeFiles/mount.myfs.dir/src/mount.myfs.c.obj"
+  "CMakeFiles/mount.myfs.dir/src/dmap.cpp.obj"
+  "CMakeFiles/mount.myfs.dir/src/fat.cpp.obj"
+  "CMakeFiles/mount.myfs.dir/src/filesystemIO.cpp.obj"
+  "bin/mount.myfs.pdb"
+  "bin/mount.myfs.exe"
+  "bin/mount.myfs.exe.manifest"
+  "lib/libmount.myfs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/mount.myfs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/root.cpp
@@ -0,0 +1,48 @@
+//
+// Created by test on 05.11.2018.
+//
+#include "constants.h"
+#include "root.h"
+
+
+
+//create new empty filestats Array
+
+root::root() {
+    this->rootArray = new fileStats[DATA_BLOCKS];
+}
+
+//return filestats of the file under given number
+fileStats root::get(uint16_t num) {
+    return this->rootArray[num];
+}
+//return full filestats array (for writing to hard driver)
+fileStats root::getAll() {
+    return this->rootArray;
+}
+//copy given filestats Array (for take data from Hard Disk)
+void root::initialize(fileStats [] newData) {
+    this->rootArray = newData;
+}
+//get filestats info from new file and add it to given position
+//in array
+bool root::set(uint16_t num, char* filePath) {
+    struct stat sb;
+    stat(path, &sb);
+    char *filename = basename(path);
+    if (strlen(filename) > NAME_LENGTH) {
+        return false;
+    }
+    fileStats status = new fileStats;
+    strcpy(status->name, filename);
+    status->size = sb.st_size;
+    status->userID = geteuid();
+    status->groupID = getegid();
+    status->modi_time = sb.st_mtime;
+    time(&(status->last_time));
+    time(&(status->change_time));
+    this->rootArray[num] = status;
+return true;
+}
+
+};
