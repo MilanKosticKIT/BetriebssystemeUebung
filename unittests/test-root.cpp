088b0a27ea78005da57e9478ca09d9b8980122bd
Hannes Illy <Hannes.Illy@gmx.de>
2018-12-02 18:34:28 +0100

    Added first tests for root.

--- unittests/test-root.cpp
+++ unittests/test-root.cpp
@@ -15,5 +15,47 @@
 #include "constants.h"
 #include "root.h"
 
-#define BD_PATH "Test_Blockdevice.bin"
+TEST_CASE("Root.setAll / Root.getAll", "[Root]"){
+    SECTION("Standard initialized Root"){
+        Root root = Root();
+        fileStats rootArray[ROOT_ARRAY_SIZE];
+        root.getAll(rootArray);
+        bool successfulInit = true;
+        for (int i = 0; i < ROOT_ARRAY_SIZE; i++) {
+            if (rootArray[i].size != -1) {
+                successfulInit = false;
+                break;
+            }
+        }
+        REQUIRE(successfulInit);
+    }
 
+    SECTION("Random Content"){
+        Root root = Root();
+        fileStats rootArray[ROOT_ARRAY_SIZE];
+        fileStats readArray[ROOT_ARRAY_SIZE];
+        root.setAll(rootArray);
+        root.getAll(readArray);
+        REQUIRE(memcmp(rootArray, readArray, ROOT_ARRAY_SIZE) == 0);
+    }
+
+}
+
+TEST_CASE("Root.createEntry / Root.get, Methods with filename as selector", "[Root]"){
+    SECTION("Standard initialized Root"){
+        Root root = Root();
+        int ret = root.createEntry("TestEntry");
+        REQUIRE(ret >= 0);
+        fileStats entry;
+        ret = root.get("TestEntry", &entry);
+        REQUIRE(ret >= 0);
+    }
+    SECTION(""){
+
+        REQUIRE(false); // todo write test
+    }
+    SECTION(""){
+
+        REQUIRE(false); // todo write test
+    }
+}
