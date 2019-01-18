b0b2891c78641a56d855075ba5192f98bcb83391
murmal <claersn@gmail.com>
2019-01-18 16:21:53 +0100

    now able to ignore std::cout log messages

--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -39,11 +39,12 @@ int main(int argc, char *argv[]) {
     DMap dmap = DMap();
     Root root = Root();
     SuperBlock superblock = {};
+    bool debug = false;
 
     if (argc > 1) {
         int ret = 0; //return value for error handling
 
-        std::cout << "Creating Blockdevice: " << argv[1] << std::endl;
+        if (debug) std::cout << "Creating Blockdevice: " << argv[1] << std::endl;
         remove(argv[1]);
         blockDevice->create(argv[1]);
 
@@ -55,29 +56,29 @@ int main(int argc, char *argv[]) {
         fat.getAll(fatArray);
         dmap.getAll(dMapArray);
         root.getAll(rootArray);
-        std::cout << "Initializing Filesystem" << std::endl;
+        if (debug) std::cout << "Initializing Filesystem" << std::endl;
 
         ret = fsIO.writeDevice(SUPERBLOCK_START, &superblock, sizeof(superblock));
         if (ret < 0) {
-            std::cout << "Error at blockdevice.write() (writing superblock):" << ret << " (description: " << errno << ")" << std::endl;
+            if (debug) std::cout << "Error at blockdevice.write() (writing superblock):" << ret << " (description: " << errno << ")" << std::endl;
         }
         ret = fsIO.writeDevice(DMAP_START, dMapArray, sizeof(*dMapArray) * (DATA_BLOCKS + 1) / 8);
         if (ret < 0) {
-            std::cout << "Error at blockdevice.write() (writing dmap):" << ret << " (description: " << errno << ")" << std::endl;
+            if (debug) std::cout << "Error at blockdevice.write() (writing dmap):" << ret << " (description: " << errno << ")" << std::endl;
         }
         ret = fsIO.writeDevice(FAT_START, fatArray, sizeof(*fatArray) * DATA_BLOCKS);
         if (ret < 0) {
-            std::cout << "Error at blockdevice.write() (writing fat):" << ret << " (description: " << errno << ")" << std::endl;
+            if (debug) std::cout << "Error at blockdevice.write() (writing fat):" << ret << " (description: " << errno << ")" << std::endl;
         }
         ret = fsIO.writeDevice(ROOT_START, rootArray, sizeof(*rootArray) * ROOT_ARRAY_SIZE);
         if (ret < 0) {
-            std::cout << "Error at blockdevice.write() (writing root):" << ret << " (description: " << errno << ")" << std::endl;
+            if (debug) std::cout << "Error at blockdevice.write() (writing root):" << ret << " (description: " << errno << ")" << std::endl;
         }
 
         if (argc > 2) {
             //When the files to copy are too large for our filesystem
             //the operation will be aborted
-            std::cout << "\nChecking if files fit in filesystem: ";
+            if (debug) std::cout << "\nChecking if files fit in filesystem: ";
             int freeSpace = DATA_BYTES;
             struct stat buffer1 = {};
             struct stat bufferTime = {};
@@ -91,19 +92,19 @@ int main(int argc, char *argv[]) {
                 }
             }
             if (sizeOK) {   //The files fit in the filesystem
-                std::cout << "The files fit in the filesystem.\n" << std::endl;
+                if (debug) std::cout << "The files fit in the filesystem.\n" << std::endl;
                 for (int i = 2; i < argc; i++) { // copy files
                     char *filename = basename(argv[i]);
-                    std::cout << "Copying file: " << argv[i] << std::endl;
+                    if (debug) std::cout << "Copying file: " << argv[i] << std::endl;
                     int fileDescriptor = open(filename, O_RDONLY);
 
                     if (fileDescriptor < 0) {
-                        std::cout << "errno: " << errno << std::endl;
+                        if (debug) std::cout << "errno: " << errno << std::endl;
                         return errno;
                     } else {
                         ret = root.createEntry(filename, 0444); // "create file"
                         if (ret < 0) {
-                            std::cout << "Root.createEntry errno: " << errno << std::endl;
+                            if (debug) std::cout << "Root.createEntry errno: " << errno << std::endl;
                             return errno;
                         }
                         fileStats stats;
@@ -112,33 +113,33 @@ int main(int argc, char *argv[]) {
                         stats.change_time = bufferTime.st_ctime;
                         root.update(stats);
                         if (ret < 0) {
-                            std::cout << "Root.get errno: " << errno << std::endl;
+                            if (debug) std::cout << "Root.get errno: " << errno << std::endl;
                             return errno;
                         }
                         uint16_t currentBlock;
                         uint16_t lastBlock;
                         ret = dmap.getFreeBlock(&currentBlock);
                         if (ret < 0) {
-                            std::cout << "DMap.getFreeBlock errno: " << errno << std::endl;
+                            if (debug) std::cout << "DMap.getFreeBlock errno: " << errno << std::endl;
                             return errno;
                         }
                         stats.first_block = currentBlock;
 
-                        std::cout << "Writing blocks ";
+                        if (debug) std::cout << "Writing blocks ";
                         char buffer[BLOCK_SIZE];
                         ssize_t retRead = read(fileDescriptor, buffer, BLOCK_SIZE);
                         while (retRead > 0) {
                             lastBlock = currentBlock;
                             ret = dmap.getFreeBlock(&currentBlock);
                             if (ret < 0) {
-                                std::cout << "\nDMap.getFreeBlock errno: " << errno << std::endl;
+                                if (debug) std::cout << "\nDMap.getFreeBlock errno: " << errno << std::endl;
                                 return errno;
                             }
-                            std::cout << currentBlock << ",";
+                            if (debug) std::cout << currentBlock << ",";
 
                             ret = blockDevice->write(DATA_START + currentBlock, buffer);
                             if (ret < 0) {
-                                std::cout << "\nBlockDevice.write errno: " << errno << std::endl;
+                                if (debug) std::cout << "\nBlockDevice.write errno: " << errno << std::endl;
                                 return errno;
                             }
                             dmap.set(currentBlock);
@@ -146,86 +147,86 @@ int main(int argc, char *argv[]) {
 
                             ret = fat.addNextToFAT(lastBlock, currentBlock);
                             if (ret < 0) {
-                                std::cout << "\nFAT.addToFAT errno: " << errno << std::endl;
+                                if (debug) std::cout << "\nFAT.addToFAT errno: " << errno << std::endl;
                                 return errno;
                             }
 
                             retRead = read(fileDescriptor, buffer, BLOCK_SIZE);
                         }
-                        std::cout << std::endl;
+                        if (debug) std::cout << std::endl;
                         fat.addLastToFAT(currentBlock);
                         ret = root.update(stats);
                         if (ret < 0) {
-                            std::cout << "Root.update errno: " << errno << std::endl;
+                            if (debug) std::cout << "Root.update errno: " << errno << std::endl;
                             return errno;
                         }
                         superblock.emptySpaceSize -= stats.size;
 
                         close(fileDescriptor);
-                        std::cout << "Finished writing file " << filename << std::endl;
+                        if (debug) std::cout << "Finished writing file " << filename << std::endl;
 
                     }
 
                 }
-                std::cout << std::endl;
+                if (debug) std::cout << std::endl;
 
-                std::cout << "Closing blockdevice: ";
+                if (debug) std::cout << "Closing blockdevice: ";
                 ret = blockDevice->close();
                 if (ret < 0) {
-                    std::cout << "failed" << std::endl;
-                    std::cout << "Error at blockdevice.close():" << ret << " (description: " << errno << ")" << std::endl;
+                    if (debug) std::cout << "failed" << std::endl;
+                    if (debug) std::cout << "Error at blockdevice.close():" << ret << " (description: " << errno << ")" << std::endl;
                 } else {
-                    std::cout << "successful!" << std::endl;
+                    if (debug) std::cout << "successful!" << std::endl;
                 }
 
-                std::cout << "Opening blockdevice: ";
+                if (debug) std::cout << "Opening blockdevice: ";
                 ret = blockDevice->open(argv[1]);
                 if (ret < 0) {
-                    std::cout << "failed" << std::endl;
-                    std::cout << "Error at blockdevice.open(" << argv[1] << "):" << ret << " (description: " << errno << ")" << std::endl;
+                    if (debug) std::cout << "failed" << std::endl;
+                    if (debug) std::cout << "Error at blockdevice.open(" << argv[1] << "):" << ret << " (description: " << errno << ")" << std::endl;
                 } else {
-                    std::cout << "successful!" << std::endl;
+                    if (debug) std::cout << "successful!" << std::endl;
                 }
 
-                std::cout << "\nWriting Filesystem data" << std::endl;
+                if (debug) std::cout << "\nWriting Filesystem data" << std::endl;
 
                 dmap.getAll(dMapArray);
                 fat.getAll(fatArray);
                 root.getAll(rootArray);
 
-                std::cout << "--------------------------------------------------------------------------------" << std::endl;
-                std::cout << "First contents of data structures:" << std::endl;
-                std::cout << "dmap:" << std::endl;
+                if (debug) std::cout << "--------------------------------------------------------------------------------" << std::endl;
+                if (debug) std::cout << "First contents of data structures:" << std::endl;
+                if (debug) std::cout << "dmap:" << std::endl;
                 for(int i = 0; i < 10; i++) {
-                    std::cout << (int)dMapArray[i] << " ";
+                    if (debug) std::cout << (int)dMapArray[i] << " ";
                 }
-                std::cout << std::endl;
-                std::cout << "fat:" << std::endl;
+                if (debug) std::cout << std::endl;
+                if (debug) std::cout << "fat:" << std::endl;
                 for(int i = 0; i < 10; i++) {
-                    std::cout << fatArray[i] << " ";
+                    if (debug) std::cout << fatArray[i] << " ";
                 }
-                std::cout << std::endl;
-                std::cout << "root (size):" << std::endl;
+                if (debug) std::cout << std::endl;
+                if (debug) std::cout << "root (size):" << std::endl;
                 for(int i = 0; i < 10; i++) {
-                    std::cout << rootArray[i].size << " ";
+                    if (debug) std::cout << rootArray[i].size << " ";
                 }
-                std::cout << std::endl;
+                if (debug) std::cout << std::endl;
 
                 ret = fsIO.writeDevice(SUPERBLOCK_START, &superblock, sizeof(superblock));
                 if (ret < 0) {
-                    std::cout << "Error at blockdevice.write() (writing superblock):" << ret << " (description: " << errno << ")" << std::endl;
+                    if (debug) std::cout << "Error at blockdevice.write() (writing superblock):" << ret << " (description: " << errno << ")" << std::endl;
                 }
                 ret = fsIO.writeDevice(DMAP_START, dMapArray, sizeof(*dMapArray) * (DATA_BLOCKS + 1) / 8);
                 if (ret < 0) {
-                    std::cout << "Error at blockdevice.write() (writing dmap):" << ret << " (description: " << errno << ")" << std::endl;
+                    if (debug) std::cout << "Error at blockdevice.write() (writing dmap):" << ret << " (description: " << errno << ")" << std::endl;
                 }
                 ret = fsIO.writeDevice(FAT_START, fatArray, sizeof(*fatArray) * DATA_BLOCKS);
                 if (ret < 0) {
-                    std::cout << "Error at blockdevice.write() (writing fat):" << ret << " (description: " << errno << ")" << std::endl;
+                    if (debug) std::cout << "Error at blockdevice.write() (writing fat):" << ret << " (description: " << errno << ")" << std::endl;
                 }
                 ret = fsIO.writeDevice(ROOT_START, rootArray, sizeof(*rootArray) * ROOT_ARRAY_SIZE);
                 if (ret < 0) {
-                    std::cout << "Error at blockdevice.write() (writing root):" << ret << " (description: " << errno << ")" << std::endl;
+                    if (debug) std::cout << "Error at blockdevice.write() (writing root):" << ret << " (description: " << errno << ")" << std::endl;
                 }
 
                 for (int i = 0; i < (DATA_BLOCKS + 1) / 8; i++) {
@@ -238,54 +239,54 @@ int main(int argc, char *argv[]) {
                     rootArray[i] = fileStats {};
                 }
 
-                std::cout << "--------------------------------------------------------------------------------" << std::endl;
-                std::cout << "Reading out first contents of data structures from blockDevice:" << std::endl;
+                if (debug) std::cout << "--------------------------------------------------------------------------------" << std::endl;
+                if (debug) std::cout << "Reading out first contents of data structures from blockDevice:" << std::endl;
 
                 ret = fsIO.readDevice(SUPERBLOCK_START, &superblock, sizeof(superblock));
                 if (ret < 0) {
-                    std::cout << "Error at blockdevice.read() (Reading superblock):" << ret << " (description: " << errno << ")" << std::endl;
+                    if (debug) std::cout << "Error at blockdevice.read() (Reading superblock):" << ret << " (description: " << errno << ")" << std::endl;
                 }
                 ret = fsIO.readDevice(DMAP_START, dMapArray, sizeof(*dMapArray) * (DATA_BLOCKS + 1) / 8);
                 if (ret < 0) {
-                    std::cout << "Error at blockdevice.read() (Reading dmap):" << ret << " (description: " << errno << ")" << std::endl;
+                    if (debug) std::cout << "Error at blockdevice.read() (Reading dmap):" << ret << " (description: " << errno << ")" << std::endl;
                 }
                 ret = fsIO.readDevice(FAT_START, fatArray, sizeof(*fatArray) * DATA_BLOCKS);
                 if (ret < 0) {
-                    std::cout << "Error at blockdevice.read() (Reading fat):" << ret << " (description: " << errno << ")" << std::endl;
+                    if (debug) std::cout << "Error at blockdevice.read() (Reading fat):" << ret << " (description: " << errno << ")" << std::endl;
                 }
                 ret = fsIO.readDevice(ROOT_START, rootArray, sizeof(*rootArray) * ROOT_ARRAY_SIZE);
                 if (ret < 0) {
-                    std::cout << "Error at blockdevice.read() (Reading root):" << ret << " (description: " << errno << ")" << std::endl;
+                    if (debug) std::cout << "Error at blockdevice.read() (Reading root):" << ret << " (description: " << errno << ")" << std::endl;
                 }
-                std::cout << "dmap:" << std::endl;
+                if (debug) std::cout << "dmap:" << std::endl;
                 for(int i = 0; i < 10; i++) {
-                    std::cout << (int)dMapArray[i] << " ";
+                    if (debug) std::cout << (int)dMapArray[i] << " ";
                 }
-                std::cout << std::endl;
-                std::cout << "fat:" << std::endl;
+                if (debug) std::cout << std::endl;
+                if (debug) std::cout << "fat:" << std::endl;
                 for(int i = 0; i < 10; i++) {
-                    std::cout << fatArray[i] << " ";
+                    if (debug) std::cout << fatArray[i] << " ";
                 }
-                std::cout << std::endl;
-                std::cout << "root (size):" << std::endl;
+                if (debug) std::cout << std::endl;
+                if (debug) std::cout << "root (size):" << std::endl;
                 for(int i = 0; i < 10; i++) {
-                    std::cout << rootArray[i].size << " ";
+                    if (debug) std::cout << rootArray[i].size << " ";
                 }
-                std::cout << std::endl;
+                if (debug) std::cout << std::endl;
 
                 delete[] dMapArray;
                 delete[] fatArray;
                 delete[] rootArray;
 
             } else {
-                std::cout << "The files to copy are to big for the filesystem. Aborting" << std::endl;
+                if (debug) std::cout << "The files to copy are to big for the filesystem. Aborting" << std::endl;
                 return errno;
             }
         }
 
         blockDevice->close();
     } else {
-        std::cout << "Invalid Arguments: Name of Containerfile missing!" << std::endl;
+        if (debug) std::cout << "Invalid Arguments: Name of Containerfile missing!" << std::endl;
         return errno = 666;
     }
     return 0;
